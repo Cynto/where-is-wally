763d86ca7a1656d6baad68f40c61d56b23ed2130
Cynto <lucagaravello@outlook.com>
2021-09-02 19:15:08 +0200

    Changed homepage

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -7,14 +7,17 @@ import 'firebase/functions';
 import CharOptions from '../charOptions/CharOptions';
 import StopWatch from '../stopWatch/StopWatch';
 import Leaderboard from '../leaderboard/Leaderboard';
+import SetUsername from '../setUsername/SetUsername';
 
 function Game(props: any) {
-  const { username, setUsername } = props;
+  const [username, setUsername] = useState('');
+
   const [xCord, setxCord] = useState(0);
   const [yCord, setyCord] = useState(0);
   const [gameComplete, setGameComplete] = useState(false);
   const [charsFound, setCharsFound] = useState(0);
   const [time, setTime] = useState(0);
+  const [timeEdit, setTimeEdit] = useState('');
 
   const db = firebase.firestore();
   const leaderboardRef = db.collection('leaderboard');
@@ -24,8 +27,7 @@ function Game(props: any) {
       let img = e.target.getBoundingClientRect();
       let x = e.clientX - img.left;
       let y = e.clientY - img.top;
-      console.log('x: ' + x);
-      console.log('y: ' + y);
+
       setxCord(x);
       setyCord(y);
 
@@ -49,18 +51,14 @@ function Game(props: any) {
   };
   useEffect(() => {
     if (charsFound === 4) {
-      console.log('0' + time);
       const timeArray = ('0' + time).split('');
-      
+
       timeArray.shift();
       timeArray.splice(-1, 1);
       const timeDot = timeArray.join('');
       console.log(timeDot);
       setGameComplete(true);
-      leaderboardRef.add({
-        username,
-        time: time / 10,
-      });
+      setTimeEdit(timeDot);
     }
   }, [charsFound]);
   useEffect(() => {
@@ -79,61 +77,79 @@ function Game(props: any) {
   }, []);
   return (
     <div className="game-page" id="game-page">
-      <StopWatch time={time} setTime={setTime} gameComplete={gameComplete} />
-      {!gameComplete ? (
-        <div className="char-container">
-          <div className="char">
-            <img
-              id="img-wally"
-              src="https://firebasestorage.googleapis.com/v0/b/where-is-wally-30eb4.appspot.com/o/waldo.jpg?alt=media&token=dcac5a4b-4465-4587-9897-0c042f59e7ba"
-              alt="Book character Wally"
-            />
-            <h4 id="text-wally">Wally</h4>
-          </div>
-          <div className="char">
-            <img
-              id="img-odlaw"
-              src="https://firebasestorage.googleapis.com/v0/b/where-is-wally-30eb4.appspot.com/o/odlaw.jpg?alt=media&token=4f89682f-0ea9-4918-83dd-03403b610ed3"
-              alt="Book character Odlaw"
-            />
-            <h4 id="text-odlaw">Odlaw</h4>
-          </div>
-          <div className="char">
-            <img
-              id="img-wizard"
-              src="https://firebasestorage.googleapis.com/v0/b/where-is-wally-30eb4.appspot.com/o/wizard.jpeg?alt=media&token=1b5ce2d0-ab6f-4cdf-b76f-ffc56bc08f2d"
-              alt="Book character Wizard"
-            />
-            <h4 id="text-wizard">Wizard</h4>
-          </div>
-          <div className="char">
-            <img
-              id="img-wenda"
-              src="https://firebasestorage.googleapis.com/v0/b/where-is-wally-30eb4.appspot.com/o/wenda.jpeg?alt=media&token=daafaf96-7bb9-45b2-ba30-5b348b2e12cd"
-              alt="Book character Wenda"
-            />
-            <h4 id="text-wenda">Wenda</h4>
-          </div>
-        </div>
-      ) : null}
-      {!gameComplete ? (
-        <div className="img-container">
-          {' '}
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/where-is-wally-30eb4.appspot.com/o/level-1.png?alt=media&token=44c0daf6-641e-4377-8393-2de606838a3f"
-            onClick={(e) => handleClick(e)}
-            alt="Where's wally"
+      {gameComplete ? (
+        <div>
+          <StopWatch
+            time={time}
+            setTime={setTime}
+            gameComplete={gameComplete}
           />
-          <div className="circle" id="circle"></div>
-          <CharOptions
-            charsFound={charsFound}
-            setCharsFound={setCharsFound}
-            xCord={xCord}
-            yCord={yCord}
-          />
+          <div className="username-container">
+            {' '}
+            <SetUsername
+              timeEdit={timeEdit}
+              username={username}
+              setUsername={setUsername}
+            />{' '}
+          </div>
         </div>
       ) : (
-        <Leaderboard />
+        <div>
+          <StopWatch
+            time={time}
+            setTime={setTime}
+            gameComplete={gameComplete}
+          />
+          <div className="char-container">
+            <div className="char">
+              <img
+                id="img-wally"
+                src="https://firebasestorage.googleapis.com/v0/b/where-is-wally-30eb4.appspot.com/o/waldo.jpg?alt=media&token=dcac5a4b-4465-4587-9897-0c042f59e7ba"
+                alt="Book character Wally"
+              />
+              <h4 id="text-wally">Wally</h4>
+            </div>
+            <div className="char">
+              <img
+                id="img-odlaw"
+                src="https://firebasestorage.googleapis.com/v0/b/where-is-wally-30eb4.appspot.com/o/odlaw.jpg?alt=media&token=4f89682f-0ea9-4918-83dd-03403b610ed3"
+                alt="Book character Odlaw"
+              />
+              <h4 id="text-odlaw">Odlaw</h4>
+            </div>
+            <div className="char">
+              <img
+                id="img-wizard"
+                src="https://firebasestorage.googleapis.com/v0/b/where-is-wally-30eb4.appspot.com/o/wizard.jpeg?alt=media&token=1b5ce2d0-ab6f-4cdf-b76f-ffc56bc08f2d"
+                alt="Book character Wizard"
+              />
+              <h4 id="text-wizard">Wizard</h4>
+            </div>
+            <div className="char">
+              <img
+                id="img-wenda"
+                src="https://firebasestorage.googleapis.com/v0/b/where-is-wally-30eb4.appspot.com/o/wenda.jpeg?alt=media&token=daafaf96-7bb9-45b2-ba30-5b348b2e12cd"
+                alt="Book character Wenda"
+              />
+              <h4 id="text-wenda">Wenda</h4>
+            </div>
+          </div>
+          <div className="img-container">
+            {' '}
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/where-is-wally-30eb4.appspot.com/o/level-1.png?alt=media&token=44c0daf6-641e-4377-8393-2de606838a3f"
+              onClick={(e) => handleClick(e)}
+              alt="Where's wally"
+            />
+            <div className="circle" id="circle"></div>
+            <CharOptions
+              charsFound={charsFound}
+              setCharsFound={setCharsFound}
+              xCord={xCord}
+              yCord={yCord}
+            />
+          </div>
+        </div>
       )}
     </div>
   );
