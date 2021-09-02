fc81754cfb7a8457f7aad1798b27e9dbbcee5a7b
Cynto <lucagaravello@outlook.com>
2021-09-02 16:22:17 +0200

    Added timer

---
+++ src/components/stopWatch/StopWatch.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import Timer from '../timer/Timer';
+import './stopWatch.css';
+
+function StopWatch(props: any) {
+  const { gameComplete, time, setTime } = props;
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+
+  React.useEffect(() => {
+    let interval: any = null;
+
+    if (isActive && isPaused === false) {
+      interval = setInterval(() => {
+        setTime((time: number) => time + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive, isPaused]);
+
+  const handleStart = () => {
+    setIsActive(true);
+    setIsPaused(false);
+  };
+  const handleReset = () => {
+    setIsActive(false);
+    setTime(0);
+  };
+
+  useEffect(() => {
+    handleStart();
+  }, []);
+  useEffect(() => {
+    if(gameComplete) {
+      setIsActive(false)
+      setIsPaused(true)
+      
+    }
+  }, [gameComplete])
+  return (
+    <div className="stop-watch">
+      <Timer time={time} />
+    </div>
+  );
+}
+
+export default StopWatch;
